37b5e9476b49e2939d3928d9d8d0af8a47d6f14c
borsodi-koppany <borsodi-koppany>
2023-12-21 18:46:08 +0100

    haladas

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-let index = 1;
+// let kepek = document.getElementsByClassName("kepnezegeto-kep");
+// let kepIndex = 0;
+// KepMutatas(kepIndex);
 
-function kepValtas(n) {
-  let i;
-  let kepek = document.querySelector(".kepvaltas-kep");
-  if (n > kepek.lenght) index = 1;
-  if (n < 1) index = kepek.lenght;
-  for (i; i < kepek.lenght; i++) {
-    kepek[i].style.display = "none";
-  }
+// function KoviKep(){
+//     kepIndex++;
+//     if(kepIndex > kepek.length) kepIndex = 0;
+//     return kepIndex;
+// }
+
+// function ElozoKep(){
+//     kepIndex--
+//     if(kepIndex < 0) kepIndex = kepek.length;
+//     return kepIndex;
+// }
+
+// function KepMutatas(n){
+//     let aktKep = kepek[n];
+//     let elozo = 0;
+//     if(n == 0) elozo = kepek[kepek.length];
+//     else elozo = kepek[n - 1];
+    
+//     elozo.style.display = "none"
+//     aktKep.style.display = "block";
+// }
+
+
+let kepIndex = 1;
+KepMutatas(kepIndex);
+
+
+function KepValtas(n){
+    let aktIndex = kepIndex += n;
+    console.log(aktIndex);
+    return aktIndex
+}
+
+
+function KepMutatas(n){
+    let kepek = document.getElementsByClassName("kepnezegeto-kep");
+    if (n > kepek.length) n = 1;
+    if (n < 1) n = kepek.length;
+    for(let i = 1; i < kepek.length; i++){
+        kepek[i].style.display = "none";
+    }
+    kepek[n - 1].style.display = "block";
 }
