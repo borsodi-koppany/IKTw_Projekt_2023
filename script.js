705d9744c9088c949a409dfb1b8a2a41fa00ccef
borsodi-koppany <borsodi-koppany>
2023-12-21 22:36:37 +0100

    mukodik, a kinezetet meg feljavitom

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
-// let kepek = document.getElementsByClassName("kepnezegeto-kep");
-// let kepIndex = 0;
-// KepMutatas(kepIndex);
+let kepIndex = 0;
 
-// function KoviKep(){
-//     kepIndex++;
-//     if(kepIndex > kepek.length) kepIndex = 0;
-//     return kepIndex;
-// }
-
-// function ElozoKep(){
-//     kepIndex--
-//     if(kepIndex < 0) kepIndex = kepek.length;
-//     return kepIndex;
-// }
-
-// function KepMutatas(n){
-//     let aktKep = kepek[n];
-//     let elozo = 0;
-//     if(n == 0) elozo = kepek[kepek.length];
-//     else elozo = kepek[n - 1];
-    
-//     elozo.style.display = "none"
-//     aktKep.style.display = "block";
-// }
-
-
-let kepIndex = 1;
-KepMutatas(kepIndex);
-
-
-function KepValtas(n){
-    let aktIndex = kepIndex += n;
-    console.log(aktIndex);
-    return aktIndex
+function Valtas(n){
+    Megjelenites(kepIndex += n)
 }
 
-
-function KepMutatas(n){
+function Megjelenites(n){
     let kepek = document.getElementsByClassName("kepnezegeto-kep");
-    if (n > kepek.length) n = 1;
-    if (n < 1) n = kepek.length;
-    for(let i = 1; i < kepek.length; i++){
+    if(n > kepek.length - 1) kepIndex = 0;
+    if(n < 0) kepIndex = kepek.length - 1;
+    for(let i = 0; i < kepek.length; i++){
         kepek[i].style.display = "none";
     }
-    kepek[n - 1].style.display = "block";
+    kepek[kepIndex].style.display = "block";
+}
+
+function Elozo(){
+    Valtas(-1);
+}
+
+function Kovetkezo(){
+    Valtas(1);
 }
